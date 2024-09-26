9c87d12fe6514089c049ed7460e2aa0eb4d89796
Nick Rukhadze <nickrukhadze@Nicks-MacBook-Pro.local>
2024-09-26 14:18:17 +0400

    fix

--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -23,7 +23,7 @@ export class UserService {
     const userEmail = email || this.configService.user.userEmail;
     const userPassword = password || this.configService.user.userPassword;
 
-    console.log("Ss")
+    console.log("sSs")
     
     if (userEmail === this.configService.user.userEmail && userPassword === this.configService.user.userPassword) {
       return {
