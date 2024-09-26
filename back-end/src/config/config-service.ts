72fc86e44980afc885aea9413407a30328ee9ca5
Nick Rukhadze <nickrukhadze@Nicks-MacBook-Pro.local>
2024-09-26 12:46:19 +0400

    fixes

--- back-end/src/config/config-service.ts
+++ back-end/src/config/config-service.ts
@@ -16,26 +16,6 @@ const slsEnv = Object.values(SLS_STAGE_ENV).includes(
 @Service()
 export class ConfigService {
 	environment: SLS_STAGE_ENV = slsEnv;
-	migrationDatabase = {
-		host: process.env.MIGRATION_DATABASE_HOST,
-		port: process.env.MIGRATION_DATABASE_PORT?parseInt(process.env.MIGRATION_DATABASE_PORT, 10) : 5432,
-		dbName: process.env.MIGRATION_DATABASE_NAME,
-		username: process.env.MIGRATION_DATABASE_USER,
-		password: process.env.MIGRATION_DATABASE_PASSWORD,
-	};
-	database = {
-		host: process.env.DATABASE_HOST,
-		port: process.env.DATABASE_PORT?parseInt(process.env.DATABASE_PORT, 10) : 5432,
-		dbName: process.env.DATABASE_NAME,
-		username: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		entityPrefix: process.env.DATABASE_ENTITY_PREFIX || "",
-	};
-	dynamoDB = {
-	dynamoDBRegion : process.env.DYNAMO_DB_REGION,
-	dynamoDBTableName : process.env.DYNAMO_DB_TABLE_NAME,
-	dynamoDBUrl : process.env.DYNAMO_DB_URL,
-	};
 	user = {
 		userEmail : process.env.USER_EMAIL,
 		userPassword : process.env.USER_PASSWORD
@@ -46,8 +26,5 @@ export class ConfigService {
 		url: process.env.FILE_SERVER_URL,
 		secretKey: process.env.FILE_SERVER_KEY,
 	};
-	
-
-
 }
 
