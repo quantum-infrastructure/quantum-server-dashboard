d84fadd766523a3f7e00a7249b1b3135c3d52878
Nick Rukhadze <nickrukhadze@Nicks-MacBook-Pro.local>
2024-09-26 12:02:08 +0400

    fixes

--- front-end/src/pages/login/index.tsx
+++ front-end/src/pages/login/index.tsx
@@ -40,7 +40,7 @@ function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
-          sign Inn
+          Sign In
         </h2>
         <form
           method="post"
