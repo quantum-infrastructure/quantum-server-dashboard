16a77692b3af4b22368d69dcafd88e63bc3ea2d4
Nick Rukhadze <nickrukhadze@Nicks-MacBook-Pro.local>
2024-09-29 17:43:42 +0400

    FFFFFFFIIIIIXXXX

--- front-end/src/pages/login/index.tsx
+++ front-end/src/pages/login/index.tsx
@@ -80,7 +80,7 @@ function Login() {
           {/* {user?.email} - - - - {sessionCheckStatus} */}
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Password1
+              Password
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
