4d59dc088daf6171470e78e26c61931c1e71b3a0
Nick Rukhadze <nickrukhadze@Nicks-MacBook-Pro.local>
2024-09-30 12:01:34 +0400

    FFFFFFFIIIIIXXXX

--- front-end/src/pages/login/index.tsx
+++ front-end/src/pages/login/index.tsx
@@ -65,7 +65,7 @@ function Login() {
         >
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Username
+              Username1
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
